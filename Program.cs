a3315a61f6970243041bdec80e3dd603e9529195
Nick Goodwill <nick.goodwill@yandex.ru>
2023-03-28 23:06:16 +0300

    main file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число 
+//и выдаёт его квадрат (число умноженное на само себя).
+
+int i = 0;
+
+if (int.TryParse(Console.ReadLine(), out i))
+{
+Console.WriteLine(i * i);
+}
+else
+{
+Console.WriteLine("OOps");
+}
